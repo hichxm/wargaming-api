f7649cc4c9e2466eaf6d75835fcef4a63f312b86
hichxm <hicham.slimani.fr@gmail.com>
2018-02-17 21:42:45 +0100

    Create WarGaming.net class file

--- src/wargaming.php
+++ src/wargaming.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Hichxm\WargamingApi;
+namespace Hichxm;
 
 
 /**
@@ -9,5 +9,38 @@ namespace Hichxm\WargamingApi;
  */
 class WargamingApi
 {
+    private $key;
+    private $region;
+
+    private $links = [
+        "accountSearch" => "api.worldoftanks.{region}/wgn/account/list/?application_id={key}&search={seach}",
+    ];
+
+    /**
+     * WargamingApi constructor.
+     * @param string $key
+     * @param string $region
+     */
+    public function __construct($key, $region)
+    {
+        $this->setKey($key);
+        $this->setRegion($region);
+    }
+
+    /**
+     * @param string $region
+     */
+    private function setRegion($region)
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * @param string $key
+     */
+    private function setKey($key)
+    {
+        $this->key = $key;
+    }
 
 }
