4b94493c1ee14118f3041ad435a364dcffaefdc2
hichxm <hicham.slimani.fr@gmail.com>
2018-02-18 12:29:16 +0100

    Test error with good api key

---
+++ tests/testWargamingApiError.php
@@ -0,0 +1,39 @@
+<?php
+
+use Hichxm\WarGaming\WargamingApi;
+use PHPUnit\Framework\TestCase;
+
+class WargamingApiErrorTest extends TestCase {
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function check_search_player_with_good_api_key_but_bad_data_option() {
+
+        //Init Wargaming.net api key and region
+        $war = new WargamingApi("e9807cace93606169c54fb8e9ec763b2", "eu");
+
+        //NOT_ENOUGH_SEARCH_LENGTH
+        try{
+            $war->searchPlayer("vo");
+        } catch (Exception $e) {
+            $this->assertEquals("NOT_ENOUGH_SEARCH_LENGTH", $e->getMessage());
+        }
+
+        //SEARCH_NOT_SPECIFIED
+        try{
+            $war->searchPlayer("");
+        } catch (Exception $e) {
+            $this->assertEquals("SEARCH_NOT_SPECIFIED", $e->getMessage());
+        }
+
+        //SEARCH_LIST_LIMIT_EXCEEDED
+        try{
+            $war->searchPlayer("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        } catch (Exception $e) {
+            $this->assertEquals("SEARCH_LIST_LIMIT_EXCEEDED", $e->getMessage());
+        }
+    }
+
+}
